a64193965c24bc419b2aeb7e101d84edff441960
Ajay Ananta Digole <ajaydigole92@gmail.com>
2024-09-12 09:01:20 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm')
+.addEventListener('submit',function(event){
+  event.preventDefault();
+  alert('thank you for your Message!. I will get back to you soon.');
+});
